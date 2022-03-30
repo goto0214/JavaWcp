91d29594196bf6d3a02c84e5e05b93bc5e9d8534
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-03-30 21:50:28 +0900

    [Update]

---
+++ src/Chapter04.java
@@ -0,0 +1,13 @@
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "後藤悠介";
+		System.out.println(str);
+		
+		str = "巨人";
+		System.out.println(str);
+		
+		final int num = 14;
+		System.out.println(num);
+	}
+
+}
