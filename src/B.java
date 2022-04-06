c14f32a5e37aac98d17329eefca20757a65a0c0a
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-04-07 01:12:31 +0900

    [Update]

---
+++ src/B.java
@@ -0,0 +1,10 @@
+
+public class B {
+	public static void main(String[] args) {
+		data("けんじ", 25);
+	}
+	
+	public static void data(String name, int age) {
+		System.out.println("私の名前は" + name + "です。" + "\n" + "年齢は" + age + "歳です。");
+	}
+}
