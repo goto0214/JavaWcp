d24143c08ce04f9b6e04c0d09372091cf4a8c93a
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-04-16 00:12:46 +0900

    [Update]

--- src/Scaner.java
+++ src/Scaner.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
+
+
 public class Scaner {
-	 public static void main(String args[]) {
-	        Scanner scanner = new Scanner(System.in);
-	        System.out.println("数値を入力してください。");
-	        int num = scanner.nextInt();	//・・・①
-	        System.out.println("入力した数値は " + num + " です。");
-	  }
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc1 = new Scanner(System.in);
+        int num1 = sc1.nextInt();
+        int num2 = sc1.nextInt();
+        
+        int total = num2;
+        for(int i = 1; i < num1; i++ ){
+            int a = sc1.nextInt();
+            total += num2 - a;
+        }
+        System.out.println(total * num2);
+    }
 }
