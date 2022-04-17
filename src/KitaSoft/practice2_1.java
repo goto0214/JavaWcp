4c47f3dd33ad8eaffd62a6590f90f39a500ab83f
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-04-17 15:18:42 +0900

    [Update]

---
+++ src/KitaSoft/practice2_1.java
@@ -0,0 +1,12 @@
+package KitaSoft;
+
+import java.util.Scanner;
+
+public class practice2_1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		
+		System.out.println(s);
+	}
+}
