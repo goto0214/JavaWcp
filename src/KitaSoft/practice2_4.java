98b53f3802293d843e5fc7e3b7073b1dacad5758
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-04-17 15:41:10 +0900

    [Update]

---
+++ src/KitaSoft/practice2_4.java
@@ -0,0 +1,17 @@
+package KitaSoft;
+
+import java.util.Scanner;
+
+public class practice2_4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		System.out.println(x + y);
+		System.out.println(x - y);
+		System.out.println(x * y);
+		System.out.println(x / y);
+		System.out.println(x % y);
+	}
+}
