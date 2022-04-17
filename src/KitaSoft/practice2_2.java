594b6092d490e84595edd2e8a786f6401dcd313a
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-04-17 15:31:23 +0900

    [Update]

---
+++ src/KitaSoft/practice2_2.java
@@ -0,0 +1,12 @@
+package KitaSoft;
+
+import java.util.Scanner;
+
+public class practice2_2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		
+		System.out.println(x);
+	}
+}
---
+++ src/KitaSoft/practice2_3.java
@@ -0,0 +1,14 @@
+package KitaSoft;
+
+import java.util.Scanner;
+
+public class practice2_3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		
+		System.out.println(Math.pow(x, 2));
+		System.out.println(Math.pow(x, 3));
+		System.out.println(Math.pow(x, 4));
+	}
+}
