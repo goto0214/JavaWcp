2d135ea2e5d8559fa8787b217f492067e3c5ae3d
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-04-17 15:57:19 +0900

    [Update]

---
+++ src/KitaSoft/practice2_5.java
@@ -0,0 +1,14 @@
+package KitaSoft;
+
+import java.util.Scanner;
+
+public class practice2_5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		System.out.println((x + y) / 2);
+	}
+
+}
---
+++ src/KitaSoft/practice2_6.java
@@ -0,0 +1,15 @@
+package KitaSoft;
+
+import java.util.Scanner;
+
+public class practice2_6 {
+	public static void main(String[] args) {
+		System.out.println("あなたの年齢を入力してください。");
+		
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		
+		System.out.println("貴方が産まれてから約" + (x * 365) + "日です");
+	}
+
+}
