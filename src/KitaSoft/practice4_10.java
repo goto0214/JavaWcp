4a23e2a11afec9dc1f21ae1eaa8c479f34ddb669
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-04-20 00:40:41 +0900

    [Update]

---
+++ src/KitaSoft/practice4_10.java
@@ -0,0 +1,13 @@
+package KitaSoft;
+
+import java.util.Scanner;
+
+public class practice4_10 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+		
+		for( int i = 0 ; i < count ; i++ )
+		    System.out.print( "*" );
+	}
+}
---
+++ src/KitaSoft/practice4_12.java
@@ -0,0 +1,14 @@
+package KitaSoft;
+
+import java.util.Scanner;
+
+public class practice4_12 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i = 0;
+		while(i <= 100) {
+			i += sc.nextInt();
+		}
+		System.out.println(i);
+	}
+}
