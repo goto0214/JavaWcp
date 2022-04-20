3b5a1b1b558244f4391ea8fa2d7c15e4b067346d
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-04-20 23:54:26 +0900

    [Update]

---
+++ src/KitaSoft/practice4_22.java
@@ -0,0 +1,15 @@
+package KitaSoft;
+
+public class practice4_22 {
+	public static void main(String[] args) {
+		int x = 0;
+		int y = 1;
+		int z = x + y;
+		while(x <= 1000) {
+			System.out.println(x);
+			x = y;
+			y = z;
+			z = x + y;
+		}
+	}
+}
