21801dc4dcf7aea9d4eb88be278a490eb432fef3
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-04-23 00:33:14 +0900

    [Update]

---
+++ src/practise_a.java
@@ -0,0 +1,13 @@
+
+public class practise_a {
+	public static void main(String[] args) {
+		int i = 1;
+		while(i < 10) {
+			for(int n = 1; n < 10; n++) {
+				System.out.print((i * n) + " ");
+			}
+			i++;
+			System.out.println();
+		}
+	}
+}
