e92015816d09a27bdb5a28bf71098097af8284ea
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-04-29 01:29:26 +0900

    [Update]

---
+++ src/KitaSoft/practice7_2.java
@@ -0,0 +1,28 @@
+package KitaSoft;
+
+class Dog1{
+	private String Name;
+	private int Age;
+	
+	public void SetName(String nm) {
+		Name = nm;
+	}
+	
+	public void SetAge(int ag){
+		Age = ag;
+	}
+	
+	public void ShowProfile() {
+		System.out.println("名前は" + Name + "、" + Age + "歳です。");
+	}
+}
+
+public class practice7_2 {
+	public static void main(String[] args) {
+		Dog1 MyDog = new Dog1();
+		
+		MyDog.SetName("ぽち");
+		MyDog.SetAge( 3 );
+		MyDog.ShowProfile();
+	}
+}
