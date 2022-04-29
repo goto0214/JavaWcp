a9bc2b82837e7b51b945006be6a06e9720374ba3
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-04-30 00:17:25 +0900

    [Update]

--- src/KitaSoft/practise7_3.java
+++ src/KitaSoft/practise7_3.java
@@ -2,6 +2,11 @@ package KitaSoft;
 class Dog2{
 	private String Name;
 	private int Age;
+	private String Kind;
+	
+	public Dog2(String kd) {
+		Kind = kd;
+	}
 	
 	public void SetName(String nm) {
 		Name = nm;
@@ -12,13 +17,13 @@ class Dog2{
 	}
 	
 	public void ShowProfile() {
-		System.out.println("名前は"+ Name + "、" + Age + "歳です。");
+		System.out.println(Kind + "で名前は"+ Name + "、" + Age + "歳です。");
 	}
 }
 public class practise7_3 {
 	public static void main(String[] args) {
-		Dog2 Dog = new Dog2();
-		Dog2 Dog1 = new Dog2();
+		Dog2 Dog = new Dog2("秋田犬");
+		Dog2 Dog1 = new Dog2("ハスキー犬");
 		
 		Dog.SetName("ぽち");
 		Dog.SetAge(3);
