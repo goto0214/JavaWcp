8677f68631ead8eee83c7b6d142d4f633c20383c
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-05-01 23:32:52 +0900

    [Update]

--- src/KitaSoft/practice8_1.java
+++ src/KitaSoft/practice8_1.java
@@ -8,11 +8,19 @@ class Animal{
 	public void ShowProfile() {
 		System.out.println("名前は" + Name + "、" + Age + "歳です。");
 	}
+	
+	public void Speak() {
+		System.out.println(".......");
+	}
 }
 
 class Cat extends Animal{
 	public void Sleep() {
 		System.out.println("スースー");
+	}
+	
+	public void Speak() {
+		System.out.println("ニャー");
 	}
 }
 
@@ -22,20 +30,22 @@ class Dog5 extends Animal
     {
         System.out.println( "トコトコ" );
     }
+    
+    public void Speak() {
+    	System.out.println("わんわん");
+    }
 }
 
 public class practice8_1 {
 	public static void main(String[] args) {
-		Cat MyCat = new Cat();
-		Dog5 MyDog = new Dog5();
+		Animal[] MyPets = new Animal[4];
+		MyPets[0] = new Cat();
+		MyPets[1] = new Dog5();
+		MyPets[2] = new Cat();
+		MyPets[3] = new Dog5();
 		
-		MyCat.Name = "たま";
-		MyCat.Age = 3;
-		MyDog.Name = "ぽち";
-		MyDog.Age = 4;
-		MyCat.ShowProfile();
-        MyDog.ShowProfile();
-        MyCat.Sleep();
-        MyDog.Run();
+		for(Animal Pet : MyPets) {
+			Pet.Speak();
+		}
 	}
 }
