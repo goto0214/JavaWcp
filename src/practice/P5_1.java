feaf4c75881c33726d6c35d424af837043681ee2
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-05-01 23:40:39 +0900

    [Update]

---
+++ src/practice/P5_1.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.util.Scanner;
+
+public class P5_1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num[] = new int[10];
+		for(int i = 0; i < 10; i++) {
+			num[i] = sc.nextInt();
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(num[i] * 2);
+		}
+	}
+}
