033b0a64a2fe0dd2daf6da425703a216de8d57ce
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-05-03 00:01:31 +0900

    [Update]

---
+++ src/practice/P5_5.java
@@ -0,0 +1,18 @@
+package practice;
+
+import java.util.Scanner;
+
+public class P5_5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int binary[] = new int[16];
+		for(int i = 15; i >= 0; i--, num /= 2) {
+			binary[i] = num % 2;
+		}
+		
+		for(int i = 0; i < 16; i++) {
+			System.out.print(binary[i]);
+		}
+	}
+}
