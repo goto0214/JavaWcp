9afae2aa07e4a9b2e479680e98f9b93086e2a7bd
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-05-03 00:14:39 +0900

    [Update]

---
+++ src/practice/P4_4.java
@@ -0,0 +1,14 @@
+package practice;
+
+import java.util.Scanner;
+
+public class P4_4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(s);
+		}
+	}
+}
