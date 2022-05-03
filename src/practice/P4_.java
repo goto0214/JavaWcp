b311bb7c3c9d28f48d79c8237ce72db067088bdb
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-05-03 23:35:49 +0900

    [Update]

---
+++ src/practice/P4_.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class P4_ {
+	public static void main(String[] args) {
+		int num = 1;
+		for(int i = 2; i <= 7; i++) {
+			num *= i;
+		}
+		System.out.println(num);
+	}
+}
---
+++ src/practice/P4_5.java
@@ -0,0 +1,14 @@
+package practice;
+
+import java.util.Scanner;
+
+public class P4_5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		for(int i = 0; i < 10; i++) {
+			sum += sc.nextInt();
+		}
+		System.out.println(sum / 10);
+	}
+}
