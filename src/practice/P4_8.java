d11e4f12720f00e77d3f2395a9b57f89832d1c63
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-05-05 22:37:09 +0900

    [Update]

---
+++ src/practice/P4_8.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.util.Scanner;
+
+public class P4_8 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = 0;
+		for(int i = 0; i < 10; i++) {
+			int n = sc.nextInt();
+			if(num < n) {
+				num = n;
+			}
+		}
+		System.out.println(num);
+	}
+}
