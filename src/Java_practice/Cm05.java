7f94fa99b1716cdd551de3de24ea14201da91cf4
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-05-07 00:48:49 +0900

    [Update]

---
+++ src/Java_practice/Cm05.java
@@ -0,0 +1,9 @@
+package Java_practice;
+
+public class Cm05 {
+	public static void main(String[] args) {
+		People05 tabira = new People05();
+		
+		System.out.println("名前は" + tabira.getName() + "で、年齢は" + tabira.getAge() + "です。");
+	}
+}
---
+++ src/Java_practice/People05.java
@@ -0,0 +1,19 @@
+package Java_practice;
+
+public class People05 {
+	private String name;
+	private int age;
+	
+	public People05() {
+		name = "田平";
+		age = 25;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+}
