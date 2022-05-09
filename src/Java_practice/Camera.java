5069f3c51acbc24b12f516473f47ef000570e68d
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-05-09 22:26:13 +0900

    [Update]

---
+++ src/Java_practice/Camera.java
@@ -0,0 +1,5 @@
+package Java_practice;
+
+public interface Camera {
+	public void takePicture();
+}
---
+++ src/Java_practice/Phone.java
@@ -0,0 +1,6 @@
+package Java_practice;
+
+public interface Phone {
+	int MAX_NUMBER_DIGITS = 11;
+	public void call(String number);
+}
