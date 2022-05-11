10ad26dc4c3e736187b87e67ae34ea5796132b78
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-05-11 23:06:08 +0900

    [Update]

---
+++ src/Java_practice/P4_17.java
@@ -0,0 +1,12 @@
+package Java_practice;
+
+public class P4_17 {
+	public static void main(String[] args) {
+		for(int i = 1; i < 10; i++) {
+			for(int x = 1; x < 10; x++) {
+				System.out.printf(" %2d", i * x);
+			}
+			System.out.println();
+		}
+	}
+}
