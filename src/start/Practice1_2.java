04a034441d7dfdde6d6bcbc4f8c1a303d6b71e00
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-05-24 23:15:24 +0900

    [Update]

---
+++ src/start/Practice1_2.java
@@ -0,0 +1,13 @@
+/*
+ * 練習問題1-2　作成日５月２４日
+ */
+
+package start;
+
+public class Practice1_2 {
+	public static void main(String[] args) {
+		int a = 3; //縦幅
+		int b = 5; //横幅
+		System.out.println("縦幅3、横幅5の長方形の面積は" + (a * b));
+	}
+}
