b1b1d333c60fdc2383c0a2c334c5d61cddea64de
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-05-24 21:32:29 +0900

    [Update]

---
+++ src/start/Constant.java
@@ -0,0 +1,13 @@
+/* 定数
+ * 　作成日　2022年5月24日
+ */
+package start;
+
+public class Constant {
+	public static void main(String[] args) {
+		final double PI = 3.14;
+		int pie = 5;
+		System.out.println("半径" + pie + "cmのパイの面積は、");
+		System.out.println(pie * pie * PI);
+	}
+}
