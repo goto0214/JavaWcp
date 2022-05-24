1e59adc869021a7232ac38e702bfa26275337439
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-05-24 23:24:36 +0900

    [Update]

---
+++ src/start/Practice1_3.java
@@ -0,0 +1,15 @@
+/*
+ * 練習問題1-3　作成日5月24日
+ */
+
+package start;
+
+public class Practice1_3 {
+	public static void main(String[] args) {
+		boolean b = true;
+		char c = '駆';
+		double d = 3.14;
+		long l = 312467997L;
+		String s = "ミナトの攻撃！敵に１５のダメージを与えた";
+	}
+}
