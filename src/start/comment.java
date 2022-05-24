4f3715259fc4caf35513a9f7214df49626d0a212
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-05-24 20:53:52 +0900

    [Update]

---
+++ src/start/comment.java
@@ -0,0 +1,14 @@
+/* Java入門
+ * 作成日 2022年5月24日
+ */
+
+package start;
+
+public class comment {
+	//ここからメインメソッド
+	public static void main(String[] args) {
+		int age; //年齢を入れる変数
+		age = 25;
+		System.out.println("私の年齢は" + age + "歳です。");
+	}
+}
