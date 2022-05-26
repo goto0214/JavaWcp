0885c357ab768fba56f387bc92477c2e12a615fa
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-05-26 16:35:06 +0900

    [Update]

--- src/start/Zu2_2.java
+++ src/start/Zu2_2.java
@@ -3,5 +3,10 @@ package start;
 public class Zu2_2 {
 	public static void main(String[] args) {
 		System.out.println("私の好きな記号は二重引用符(\")です。");
+		
+		float f = 3;
+		double d = f;
+		System.out.println(f);
+		System.out.println(d);
 	}
 }
