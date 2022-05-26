83558de37f6f8284156ee473f83fb098f0c9686a
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-05-26 16:42:01 +0900

    [Update]

--- src/start/Zu2_2.java
+++ src/start/Zu2_2.java
@@ -9,7 +9,12 @@ public class Zu2_2 {
 		System.out.println(f);
 		System.out.println(d);
 		
-		int age = (int)3.2;
+		int age = (int)3.2;//よほどの事が無い限り、この強制型変換は使わない。
 		System.out.println(age);
+		
+		d = 8.5 / 2;
+		long l = 5 + 2L;
+		System.out.println(d);
+		System.out.println(l);
 	}
 }
