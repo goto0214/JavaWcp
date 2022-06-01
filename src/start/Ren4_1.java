0a71ec01e70488998cab29c1554417da07d752d1
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-06-01 22:07:00 +0900

    [Update]

---
+++ src/start/Ren4_1.java
@@ -0,0 +1,15 @@
+package start;
+
+
+public class Ren4_1 {
+	public static void main(String[] args) {
+		int[] points = {10, 20, 30, 40};
+		double[] weight = {1.5, 2.6, 3.7, 1.6, 8.0};
+		boolean[] answers = {true, false, true};
+		String[] names = {"ユウスケ", "かかｋ", "あああ"};
+		
+		for(int i = 0; i < points.length; i++) {
+			System.out.println(points[i]);
+		}
+	}
+}
