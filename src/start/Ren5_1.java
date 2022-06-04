6091d93add1a1a82957020459bcddcade858c959
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-06-04 22:35:21 +0900

    [Update]

---
+++ src/start/Ren5_1.java
@@ -0,0 +1,19 @@
+package start;
+
+public class Ren5_1 {
+	public static void main(String[] args) {
+		introduceOnself();
+	}
+	
+	public static void introduceOnself() {
+		String name = "後藤悠介";
+		int age = 25;
+		double height = 173.5;
+		char zodiac = '神';
+		
+		System.out.println("名前は" + name);
+		System.out.println("年齢は" + age);
+		System.out.println("身長は"+ height);
+		System.out.println("干支は" + zodiac);
+	}
+}
