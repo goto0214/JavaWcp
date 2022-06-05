6c302c13d3081b8e7839ab317c9f9200f59982c0
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-06-06 00:00:26 +0900

    [Update]

---
+++ src/start/Code6_10.java
@@ -0,0 +1,11 @@
+package start;
+
+public class Code6_10 {
+	public static void main(String[] args) {
+		int[] heights = {186, 42, 355, 46, 555};
+		java.util.Arrays.sort(heights);
+		for(int n : heights) {
+			System.out.println(n);
+		}
+	}
+}
