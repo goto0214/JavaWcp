80ca3dbb6671899da4ce1bc8655eebe79da47b1b
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-06-05 20:16:59 +0900

    [Update]

--- src/start/Ren5_2.java
+++ src/start/Ren5_2.java
@@ -9,12 +9,19 @@ public class Ren5_2 {
 		System.out.println("本文：" + text);
 	}
 	
+	public static void email(String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました。");
+		System.out.println("件名：無題");
+		System.out.println("本文：" + text);
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String address = sc.nextLine();
 		String title = sc.nextLine();
 		String text = sc.nextLine();
-		
+		sc.close();
 		email(title, address, text);
+		email(address, text);
 	}
 }
