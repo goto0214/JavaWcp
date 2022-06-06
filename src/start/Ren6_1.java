dabf8dfb14ac25e385332c6e2b9921b5fe99f62a
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-06-06 21:20:37 +0900

    [Update]

---
+++ src/start/Ren6_1.java
@@ -0,0 +1,13 @@
+package start;
+
+import comment.Zenhan;
+
+public class Ren6_1 {
+	public static void main(String[] args) throws Exception{
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		start.Kouhan.callDeae();
+		start.Kouhan.showMondokoro();
+	}
+	
+}
