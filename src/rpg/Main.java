cf47337f65e5d84199cbc7d1ce3c29e220a367e2
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-06-07 21:08:49 +0900

    [Update]

--- src/rpg/Main.java
+++ src/rpg/Main.java
@@ -10,12 +10,18 @@ public class Main {
 		int[] yourStatus = new int[5];
 		String[] status = {"HP", "ATK", "DEF", "SPD", "LUK"};
 		System.out.println("貴方のステータスを決めます。リセマラできます。");
+		for(int a = 0; a < 1;) {
 			Status.status(yourStatus);
 			for(int i = 0; i < 5; i++) {
 				System.out.println(status[i] + ": " + yourStatus[i]);
 			}
-			System.out.println("このステータスでいいでしょうか？？OKなら数字の1を入力してください。NGならそれ以外の数字を入力してください。");
-			
+			System.out.println("このステータスでいいでしょうか？？");
+			System.out.println("OKなら数字の1を入力してください。NGならそれ以外の数字を入力してください。");
+			int i = sc.nextInt();
+			if(i == 1) {
+				a++;
+			}
+		}
 		
 	}
 }
