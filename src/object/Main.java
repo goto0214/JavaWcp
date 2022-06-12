6d2feb672828cce190969d17ff94d4b62bfaadb0
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-06-13 00:20:16 +0900

    [Update]8章練習問題

--- src/object/Main.java
+++ src/object/Main.java
@@ -6,6 +6,19 @@ public class Main {
 		
 		h.name = "ミナト";
 		h.hp = 100;
-		System.out.println("勇者" + h.name + "を生み出しました！");
+		
+		Matango m1 = new Matango();
+		m1.hp = 50;
+		m1.suffix = 'A';
+		
+		Matango m2 = new Matango();
+		m2.hp = 48;
+		m2.suffix = 'B';
+		
+		
+		h.slip();
+		m1.run();
+		m2.run();
+		h.run();
 	}
 }
